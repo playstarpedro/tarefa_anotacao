06967526564d494c292e60edb0ffec4be5bbacfc
pedroDev <playstarpedro@gmail.com>
2024-07-28 13:46:36 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+
+    @Tabela(value = "Nome da Tabela")
+    public static void main(String[] args) {
+    }
+}
---
+++ src/Tabela.java
@@ -0,0 +1,9 @@
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Documented
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface Tabela {
+    String value();
+}
